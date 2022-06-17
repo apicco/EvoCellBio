af86ff76a0e6ae64a7e128155f8bf535ddcf06e0
Andrea Picco <apicco@gmail.com>
2022-06-17 15:39:16 +0200

    add new fim1_lifetimes_control.py script

---
+++ Protein_lifetimes/fim1_lifetimes_control.py
@@ -0,0 +1,100 @@
+from matplotlib import pyplot as plt
+from matplotlib import patches
+import numpy as np
+import copy as cp
+from data import *
+   
+def flt( species , protein , d , dt , RFP = True ) :
+   
+    if RFP :
+        l = ( d.RFP_end - d.RFP_start + 1 ) * dt
+    else :
+        l = ( d.GFP_end - d.GFP_start + 1 ) * dt
+   
+    # averages
+    ml = [ avg( l ) , err( l ) ]
+
+    d = pd.DataFrame( [[ protein , np.round( ml[0] , 2 ) , np.round( ml[1] , 2 ) ]] , columns = [ 'Protein' , 'Fim1 lifetime (s)' , 'SD (s)' ] , index = [ species ] )
+
+    return d 
+
+def avg( x ) :
+    return np.mean( x )
+    #return np.median( x )
+
+def err( x , k = 1.4826 ) :
+    return np.std( x )
+    #return k * np.median( np.abs( x - np.median( x ) ) )
+
+# lifetimes
+species = 'S. cerevisiae'
+
+Ede1_sc_l = flt( species , 'Ede1' , Ede1_sc , dt = 1.2 )
+Pan1_sc_l = flt( species , 'Pan1' , Pan1_sc , dt = 1.2 )
+data = pd.concat( [ Ede1_sc_l , Pan1_sc_l ] )
+Sla1_sc_l = flt( species , 'Sla1' , Sla1_sc , dt = 1.2 )
+data = pd.concat( [ data , Sla1_sc_l ] )
+Wasp_sc_l = flt( species , 'Wasp' , Wasp_sc , dt = 1.2 )
+data = pd.concat( [ data , Wasp_sc_l ] )
+Myo3_sc_l = flt( species , 'Myo3' , Myo3_sc , dt = 1.2 )
+data = pd.concat( [ data , Myo3_sc_l ] )
+Myo5_sc_l = flt( species , 'Myo5' , Myo5_sc , dt = 1.2 )
+data = pd.concat( [ data , Myo5_sc_l ] )
+Rvs_sc_l = flt( species , 'Rvs167' , Rvs_sc , dt = 1.19 )
+data = pd.concat( [ data , Rvs_sc_l ] )
+Arc18_sc_l = flt( species , 'Arc18' , Arc18_sc , dt = 1.2 )
+data = pd.concat( [ data , Arc18_sc_l ] )
+Fim1_GFP_sc_l = flt( species , 'Fim1-GFP' , Fim1_GFP_sc , dt = 1.9 , RFP = False )
+data = pd.concat( [ data , Fim1_GFP_sc_l ] )
+Fim1_RFP_sc_l = flt( species , 'Fim1-RFP' , Fim1_RFP_sc , dt = 1.9 )
+data = pd.concat( [ data , Fim1_RFP_sc_l ] )
+
+species = 'S. pombe'
+
+Ede1_sp_Ucp8_l = flt( species , 'Ucp8' , Ede1_sp_Ucp8 , dt = 1.2 )
+data = pd.concat( [ data , Ede1_sp_Ucp8_l ] )
+Ede1_sp_l = flt( species , 'Ede1' , Ede1_sp , dt = 1.2 )
+data = pd.concat( [ data , Ede1_sp_l ] )
+Pan1_sp_l = flt( species , 'Pan1' , Pan1_sp , dt = 1.2 )
+data = pd.concat( [ data , Pan1_sp_l ] )
+Sla1_sp_l = flt( species , 'Sla1' , Sla1_sp , dt = 1.2 )
+data = pd.concat( [ data , Sla1_sp_l ] )
+Wasp_sp_l = flt( species , 'Wasp' , Wasp_sp , dt = 1.2 )
+data = pd.concat( [ data , Wasp_sp_l ] )
+Myo1_sp_l = flt( species , 'Myo1' , Myo1_sp , dt = 1.2 )
+data = pd.concat( [ data , Myo1_sp_l ] )
+Rvs_sp_l = flt( species , 'Rvs167' , Rvs_sp , dt = 1.19 )
+data = pd.concat( [ data , Rvs_sp_l ] )
+Arc18_sp_l = flt( species , 'Arc18' , Arc18_sp , dt = 1.2 )
+data = pd.concat( [ data , Arc18_sp_l ] )
+Fim1_GFP_sp_l = flt( species , 'Fim1-GFP' , Fim1_GFP_sp , dt = 1.9 , RFP = False )
+data = pd.concat( [ data , Fim1_GFP_sp_l ] )
+Fim1_RFP_sp_l = flt( species , 'Fim1-RFP' , Fim1_RFP_sp , dt = 1.9 )
+data = pd.concat( [ data , Fim1_RFP_sp_l ] )
+
+species = 'U. maydis'
+
+Ede1_um_l = flt( species , 'Ede1' , Ede1_um , dt = 1.2 )
+data = pd.concat( [ data , Ede1_um_l ] )
+Pan1_um_l = flt( species , 'Pan1' , Pan1_um , dt = 1.2 )
+data = pd.concat( [ data , Pan1_um_l ] )
+Sla1_um_l = flt( species , 'Sla1' , Sla1_um , dt = 1.2 )
+data = pd.concat( [ data , Sla1_um_l ] )
+Wasp_um_l = flt( species , 'Wasp' , Wasp_um , dt = 1.2 )
+data = pd.concat( [ data , Wasp_um_l ] )
+Myo1_um_l = flt( species , 'Myo1' , Myo1_um , dt = 1.2 )
+data = pd.concat( [ data , Myo1_um_l ] )
+Rvs_um_l = flt( species , 'Rvs167' , Rvs_um , dt = 1.19 )
+data = pd.concat( [ data , Rvs_um_l ] )
+Arc18_um_l = flt( species , 'Arc18' , Arc18_um , dt = 1.2 )
+data = pd.concat( [ data , Arc18_um_l ] )
+Fim1_GFP_um_l = flt( species , 'Fim1-GFP' , Fim1_GFP_um , dt = 1.9 , RFP = False )
+data = pd.concat( [ data , Fim1_GFP_um_l ] )
+Fim1_RFP_um_l = flt( species , 'Fim1-RFP' , Fim1_RFP_um , dt = 1.9 )
+data = pd.concat( [ data , Fim1_RFP_um_l ] )
+
+print( data )
+#f = plt.figure()
+#data.plot.bar( x = 'Protein' , y = 'Fim1 lifetime (s)' , rot = 0 ) 
+#plt.savefig( 'Fim1_lifetime_control.pdf' )
+
