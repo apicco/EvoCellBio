67ab40c0b55ac90ec73ebb3d4c21d0885623b6fa
apicco <andrea.picco@unige.ch>
2022-07-18 16:28:48 +0200

    zoom plot for the first 3 seconds

--- Protein_lifetimes/lifetimes.py
+++ Protein_lifetimes/lifetimes.py
@@ -115,3 +115,76 @@ plt.tight_layout()
 plt.savefig( "Protein_lifetimes.pdf" )
 
 plt.figure()
+
+
+###### PLOT THE ZOOM #######
+
+def layout_zoom( ax , title ) :
+
+    ax.plot( ( 0 , 0 ) , ( -100 , 100 ) , 'k--' , lw = 0.5 )
+    ax.set_ylim( -22 , 3 )
+    ax.set_xlim( -0.5 , 3.5 )
+    ax.yaxis.tick_right()
+    ax.set_yticks( [ 1 , -2 , -5 , -8 , -11 , -14 , -17 , -20 ] )
+    ax.set_ylabel( title , fontsize = 18 , style = 'italic' )
+    ax.set_xticks( [ 0 , 2 , 3 ] )
+    ax.grid( axis = 'x' )
+
+fig , ax = plt.subplots( 3 , 1 , figsize = ( 2.5 , 11 ) , sharex = 'all' )
+
+sc = ax[ 0 ]
+sp = ax[ 1 ]
+um = ax[ 2 ]
+
+# invagination start
+is_sc = Is( sc , I_sc , dt = 0.7 , do_plot = True )
+shift_sc = - is_sc[ 0 ]
+# lifetimes
+lt( sc , Ede1_sc , -0 , 2 , dt = 1.2 , shift = shift_sc , col = color_Ede1 )
+lt( sc , Pan1_sc , -3 , 2 , dt = 1.2 , shift = shift_sc , col = color_Pan1 )
+lt( sc , Sla1_sc , -6 , 2 , dt = 1.2 , shift = shift_sc , col = color_Sla1 )
+lt( sc , Wasp_sc , -9 , 2 , dt = 1.2 , shift = shift_sc , col = color_Wasp )
+lt( sc , Myo3_sc , -12 , 2 , dt = 1.2 , shift = shift_sc , col = color_Myo3 )
+lt( sc , Myo5_sc , -15 , 2 , dt = 1.2 , shift = shift_sc , col = color_Myo5 )
+lt( sc , Rvs_sc , -18 , 2 , dt = 1.19 , shift = shift_sc , col = color_Rvs )
+lt( sc , Fim1_GFP_sc , -21 , 2 , dt = 1.19 , shift = shift_sc , col = color_Fim1 , is_t0 = False )
+
+layout_zoom( sc , 'S. cerevisiae' )
+sc.set_yticklabels( [ 'Ede1' , 'Pan1' , 'Sla1' , 'Wasp', 'Myo3' , 'Myo5' , 'Rvs167' , 'Fim1' ] )
+
+# invagination start
+is_sp = Is( sp , I_sp , dt = 0.71 , do_plot = True )
+shift_sp = - is_sp[ 0 ]
+# lifetimes
+lt( sp , Ede1_sp_Ucp8 , -0 , 2 , dt = 1.2 , shift = shift_sp , col = color_Ede1_Ucp8 )
+lt( sp , Ede1_sp , -3 , 2 , dt = 1.2 , shift = shift_sp , col = color_Ede1 )
+lt( sp , Pan1_sp , -6 , 2 , dt = 1.2 , shift = shift_sp , col = color_Pan1 )
+lt( sp , Sla1_sp , -9 , 2 , dt = 1.2 , shift = shift_sp , col = color_Sla1 )
+lt( sp , Wasp_sp , -12 , 2 , dt = 1.2 , shift = shift_sp , col = color_Wasp )
+lt( sp , Myo1_sp , -15 , 2 , dt = 1.2 , shift = shift_sp , col = color_Myo1 )
+lt( sp , Rvs_sp , -18 , 2 , dt = 1.19 , shift = shift_sp , col = color_Rvs )
+lt( sp , Fim1_GFP_sp , -21 , 2 , dt = 1.19 , shift = shift_sp , col = color_Fim1 , is_t0 = False )
+layout_zoom( sp , 'S. pombe' )
+sp.set_yticklabels( [ 'Ucp8' , 'Ede1' , 'Pan1' , 'Sla1' , 'Wasp' , 'Myo1' , 'Rvs167' , 'Fim1' ] )
+
+# invagination start
+is_um = Is( um , I_um , dt = 0.71 , do_plot = True )
+shift_um = - is_um[ 0 ]
+# lifetimes
+lt( um , Ede1_um , -0 , 2 , dt = 1.2 , shift = shift_um , col = color_Ede1 )
+lt( um , Pan1_um , -3 , 2 , dt = 1.2 , shift = shift_um , col = color_Pan1 )
+lt( um , Sla1_um , -6 , 2 , dt = 1.2 , shift = shift_um , col = color_Sla1 )
+lt( um , Wasp_um , -9 , 2 , dt = 1.2 , shift = shift_um , col = color_Wasp )
+lt( um , Myo1_um , -12 , 2 , dt = 1.2 , shift = shift_um , col = color_Myo1 )
+lt( um , Rvs_um , -15 , 2 , dt = 1.19 , shift = shift_um , col = color_Rvs )
+lt( um , Fim1_GFP_um , -18 , 2 , dt = 1.19 , shift = shift_um , col = color_Fim1 , is_t0 = False )
+layout_zoom( um , 'U. maydis' )
+um.set_yticklabels( [ 'Ede1' , 'Pan1' , 'Sla1' , 'Wasp' , 'Myo1' , 'Rvs167' , 'Fim1' , '' ] )
+
+plt.xlabel( 'Time (s)' , fontsize = 18 )
+plt.tight_layout()
+plt.savefig( "Protein_lifetimes_zoom.pdf" )
+
+plt.figure()
+
+
