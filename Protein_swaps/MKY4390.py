804033230240a276d2801ea0326c00ebfbe9bc0e
apicco <andrea.picco@unige.ch>
2022-07-25 22:18:00 +0200

    add aligned csv, nad par option as par.py

--- Protein_swaps/MKY4390.py
+++ Protein_swaps/MKY4390.py
@@ -1,31 +1,22 @@
 from funs import *
-import pandas as pd
+from par import *
 import os
 import numpy as np
 path = 'MKY4390'
 
 data = sorted( os.listdir( path ) )
-RFP_data = [ d for d in data if 'RFP' in d ]
-GFP_data = [ d for d in data if 'GFP' in d ]
+
+RFP_data = [ path + '/' + d for d in data if 'RFP' in d and 'aligned' not in d ]
+GFP_data = [ path + '/' + d for d in data if 'GFP' in d and 'aligned' not in d ]
 
 lr = len( RFP_data )
 lg = len( GFP_data )
 
-r = []
-g = []
 s = np.zeros( lr ) #shifts
 s[ 4 ] = -2 
 s[ 2 ] = 8 
 s[ 1 ] = 5
 s[ 0 ] = 12
-if lr == lg :
 
-    for i in range( lr ) :
-
-        print( RFP_data[ i ] + ' and ' + GFP_data[ i ] )
-
-        r.append( pd.read_csv( path + '/' + RFP_data[ i ] ) )
-        g.append( pd.read_csv( path + '/' + GFP_data[ i ] ) )
-
-plot( r , g , s , dt = 1.1948 , output = 'MKY4390.pdf' , legend = False )
+plot( RFP_data , GFP_data , s , dt = 1.1948 , output = 'MKY4390.pdf' , shift = s_MKY4390 , legend = False , xlim = xlim )
 #plot( r , g , s , dt = 1.1948 , output = 'tmp.pdf' , legend = True )
