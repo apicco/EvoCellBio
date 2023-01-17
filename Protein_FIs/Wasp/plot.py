df580e9adf1efa719af619ca0376354571e77ca7
apicco <andrea.picco@unige.ch>
2023-01-17 18:21:53 +0100

    add MKY4390 and split plot in two pdfs

--- Protein_FIs/Wasp/plot.py
+++ Protein_FIs/Wasp/plot.py
@@ -17,6 +17,14 @@ rfp_4794 = load_directory( 'Data/221111_MKY4794_TIRF/' , pattern = 'RFP.txt' , d
 
 g_4794 , r_4794 = cc_2colors( gfp_4794 , rfp_4794 )
 
+# load GFP trajectories and time them
+gfp_4390 = load_directory( 'Data/220712_MKY4390_TIRF/' , pattern = 'GFP.txt' , dt = 1.2432 , t_unit = 's' , comment_char = '%' , frames = 0 , f = 3 , intensity_normalisation = 'Absolute' )
+
+# load RFP trajectories and time them
+rfp_4390 = load_directory( 'Data/220712_MKY4390_TIRF/' , pattern = 'RFP.txt' , dt = 1.2432 , t_unit = 's' , comment_char = '%' , frames = 0 , f = 3 , intensity_normalisation = 'Integral' )
+
+g_4390 , r_4390 = cc_2colors( gfp_4390 , rfp_4390 )
+
 f , ax = plt.subplots( 2 , 2 , figsize = ( 8 , 4 ) , sharex = True )
 
 plot_all( ax[0,0] , ax[1,0] , g_4801 , r_4801 , g_label = '$Sc_{Wasp}$' , r_label = 'Fim1' )
@@ -33,4 +41,22 @@ ax[0,0].set_title( '$sla1\Delta$' )
 ax[0,1].set_title( '$sla1\Delta$' )
 
 plt.tight_layout()
+plt.savefig( 'plot_sla1delta.pdf' )
+
+f , ax = plt.subplots( 2 , 2 , figsize = ( 8 , 4 ) , sharex = True )
+
+plot_all( ax[0,0] , ax[1,0] , g_4390 , r_4390 , g_label = '$Sp_{Wasp}$' , r_label = 'Fim1' )
+
+# set common xlim
+xlim = ( -50 , 30 )
+ax[0,0].set_xlim( xlim )
+ax[0,1].set_xlim( xlim )
+ax[1,0].set_xlim( xlim )
+ax[1,1].set_xlim( xlim )
+
+ax[0,0].set_title( '$WT$' )
+ax[0,1].set_title( '$WT$' )
+
+plt.tight_layout()
 plt.savefig( 'plot.pdf' )
+
