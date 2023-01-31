ca1efe2090cb01f400c0c380c5a24792b4ebf1df
Andrea Picco <apicco@gmail.com>
2023-01-31 14:10:21 +0100

    add Pombe FI profiles

--- Protein_FIs/Wasp/plot.py
+++ Protein_FIs/Wasp/plot.py
@@ -3,7 +3,7 @@ from funs import *
 
 #------ LOAD DATA 
 
-#--- Sc_Wasp and Fim1 (WT)
+#--- Sc_Wasp and Fim1 in Sc (WT)
 
 # load GFP trajectories and time them
 gfp_4021 = load_directory( 'Data/220506_MKUM24_MKYP26_MKY4021/Cerevisiae/' , pattern = 'GFP.txt' , dt = 1.2 , t_unit = 's' , comment_char = '%' , frames = 0 , f = 3 , intensity_normalisation = 'Absolute' )
@@ -13,7 +13,17 @@ rfp_4021 = load_directory( 'Data/220506_MKUM24_MKYP26_MKY4021/Cerevisiae/' , pat
 
 g_4021 , r_4021 = cc_2colors( gfp_4021 , rfp_4021 )
 
-#--- Sp_Wasp and Fim1
+#--- Sp_Wasp and Fim1 in Sp (WT)
+
+# load GFP trajectories and time them
+gfp_0026 = load_directory( 'Data/220506_MKUM24_MKYP26_MKY4021/Pombe/' , pattern = 'GFP.txt' , dt = 1.2 , t_unit = 's' , comment_char = '%' , frames = 0 , f = 3 , intensity_normalisation = 'Absolute' )
+
+# load RFP trajectories and time them
+rfp_0026 = load_directory( 'Data/220506_MKUM24_MKYP26_MKY4021/Pombe/' , pattern = 'RFP.txt' , dt = 1.2 , t_unit = 's' , comment_char = '%' , frames = 0 , f = 3 , intensity_normalisation = 'Integral' )
+
+g_0026 , r_0026 = cc_2colors( gfp_0026 , rfp_0026 )
+
+#--- Sp_Wasp and Fim1 in Sc
 
 # load GFP trajectories and time them
 gfp_4390 = load_directory( 'Data/220712_MKY4390_TIRF/' , pattern = 'GFP.txt' , dt = 1.2432 , t_unit = 's' , comment_char = '%' , frames = 0 , f = 3 , intensity_normalisation = 'Absolute' )
@@ -23,7 +33,7 @@ rfp_4390 = load_directory( 'Data/220712_MKY4390_TIRF/' , pattern = 'RFP.txt' , d
 
 g_4390 , r_4390 = cc_2colors( gfp_4390 , rfp_4390 )
 
-#--- Sc_Wasp and Fim1 in sla1delta
+#--- Sc_Wasp and Fim1 in sla1delta in Sc
 
 # load GFP trajectories and time them
 gfp_4801 = load_directory( 'Data/221111_MKY4801_TIRF/' , pattern = 'GFP.txt' , dt = 1.2432 , t_unit = 's' , comment_char = '%' , frames = 0 , f = 3 , intensity_normalisation = 'Absolute' )
@@ -33,7 +43,7 @@ rfp_4801 = load_directory( 'Data/221111_MKY4801_TIRF/' , pattern = 'RFP.txt' , d
 
 g_4801 , r_4801 = cc_2colors( gfp_4801 , rfp_4801 )
 
-#--- Sp_Wasp and Fim1 in sla1delta
+#--- Sp_Wasp and Fim1 in sla1delta in Sc
 
 # load GFP trajectories and time them
 gfp_4794 = load_directory( 'Data/221111_MKY4794_TIRF/' , pattern = 'GFP.txt' , dt = 1.2432 , t_unit = 's' , comment_char = '%' , frames = 0 , f = 3 , intensity_normalisation = 'Absolute' )
@@ -43,7 +53,7 @@ rfp_4794 = load_directory( 'Data/221111_MKY4794_TIRF/' , pattern = 'RFP.txt' , d
 
 g_4794 , r_4794 = cc_2colors( gfp_4794 , rfp_4794 )
 
-#--- PLOT THE WT
+#--- PLOT THE WT in Sc
 
 f , ax = plt.subplots( 2 , 2 , figsize = ( 8 , 4 ) , sharex = True )
 
@@ -57,11 +67,32 @@ ax[0,1].set_xlim( xlim )
 ax[1,0].set_xlim( xlim )
 ax[1,1].set_xlim( xlim )
 
+ax[0,0].set_title( '$Sp, WT$' )
+ax[0,1].set_title( '$Sc, WT$' )
+
+plt.tight_layout()
+plt.savefig( 'plot_Sc.pdf' )
+
+#--- PLOT THE WT in Sp and Um
+
+f , ax = plt.subplots( 2 , 2 , figsize = ( 8 , 4 ) , sharex = True )
+
+plot_all( ax[0,0] , ax[1,0] , g_0026 , r_0026 , g_label = '$Sp_{Wasp}$' , r_label = 'Fim1' , alpha = 0.1 )
+#plot_all( ax[0,1] , ax[1,1] , g_4021 , r_4021 , g_label = '$Um_{Wasp}$' , r_label = 'Fim1' , alpha = 0.1 )
+
+# set common xlim
+xlim = ( -50 , 30 )
+ax[0,0].set_xlim( xlim )
+ax[0,1].set_xlim( xlim )
+ax[1,0].set_xlim( xlim )
+ax[1,1].set_xlim( xlim )
+
 ax[0,0].set_title( '$Sc, WT$' )
 ax[0,1].set_title( '$Sc, WT$' )
 
 plt.tight_layout()
 plt.savefig( 'plot.pdf' )
+
 
 #--- PLOT sla1delta
 
