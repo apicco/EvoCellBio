445e66243a426c442aa01896002865f5d6b95bb0
apicco <andrea.picco@unige.ch>
2023-02-22 14:16:03 +0100

    add 27 degree raw data and averages

---
+++ Protein_tracking/Data/Sla1/27_degree/Sp/230127_MKYP0006_27deg.py
@@ -0,0 +1,22 @@
+from trajalign.traj import Traj
+from trajalign.average import load_directory
+from trajalign.average import average_trajectories
+
+trajectory_list = load_directory(
+		path = 'raw_data' , 
+		pattern = '.shd1.txt' ,
+		comment_char = '%' , 
+		dt = 0.1008 , #ms 
+		t_unit = 's' , 
+		coord_unit = 'pxl' , 
+		frames = 0 , 
+		coord = ( 1 , 2 ) , 
+		f = 3 , 
+		protein = 'Shd1-GFP' , 
+		date = '27/01/23' , 
+		notes = 'in EMM media, at 27 deg C')
+
+best_median , worst_median , aligned_trajectories_median =\
+		average_trajectories( trajectory_list , max_frame = 600 , 
+				output_file = 'sla1_sp_27deg' , median = True ) 
+
---
+++ Protein_tracking/Data/Sla1/27_degree/Um/raw_data/readme.txt
@@ -0,0 +1,3 @@
+traj Traj_220127_MKUM0001_MKYP0034_MKY2833_l488-32_g255_100ms__8_MMStack_Pos0.ome_BGN_MD.txt
+and trajectories 16.usla1.txt - 18.usla1.txt
+to be confirmed as um
