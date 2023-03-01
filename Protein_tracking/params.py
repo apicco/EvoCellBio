28fcad5c9b71d0ad029ce5ae06951ae6445aaad0
apicco <andrea.picco@unige.ch>
2023-03-01 13:53:42 +0100

    add temperature trajectory plots by species

--- Protein_tracking/params.py
+++ Protein_tracking/params.py
@@ -19,57 +19,63 @@ movlim = {
     "D" : ( -55 , 450 ) ,
     "E" : ( -55 , 450 )
 }
+species_tlim = {
+    "A" : ( -31 , 22 ) , #50
+    "B" : ( -15 , 20 ) , #35
+    "C" : ( -25 , 10 ) , #35
+}
+
 # Shifts to align the different trajectories
 
 # at 18 degree
 # in space
 x0_sc_18deg = -0.03
-x0_pb_18deg = -0.26
+x0_sp_18deg = -0.26
 x0_um_18deg = -0.01
 # and in time
 t0_sc_18deg = unified_start( sla1_sc_18 ) + 27.1
-t0_pb_18deg = unified_start( sla1_sp_18 ) + 11.2
-t0_um_18deg = unified_start( sla1_um_18 ) + 21.7 
+t0_sp_18deg = unified_start( sla1_sp_18 ) + 10.8
+t0_um_18deg = unified_start( sla1_um_18 ) + 21.4 
 
 # at 21 degree
 # in space
 x0_sc_21deg = -0.03
-x0_pb_21deg = -0.26
+x0_sp_21deg = -0.26
 x0_um_21deg = -0.01
 # and in time
 t0_sc_21deg = unified_start( sla1_sc_21 ) + 20.3
-t0_pb_21deg = unified_start( sla1_sp_21 ) + 7
+t0_sp_21deg = unified_start( sla1_sp_21 ) + 7
 t0_um_21deg = unified_start( sla1_um_21 ) + 11.3 
 
 # at 24 degree
 # in space
 x0_sc_24deg = -0.03
-x0_pb_24deg = -0.20
+x0_sp_24deg = -0.20
 x0_um_24deg = -0.01
 # and in time
 t0_sc_24deg = unified_start( sla1_sc_24 ) + 16
-t0_pb_24deg = unified_start( sla1_sp_24 ) + 7.5
+t0_sp_24deg = unified_start( sla1_sp_24 ) + 7.5
 t0_um_24deg = unified_start( sla1_um_24 ) + 17.5
 
 # at 27 degree
 # in space
-x0_sc_27deg = -0.03
-x0_pb_27deg = -0.26
+x0_sc_27deg = -0.06
+x0_sp_27deg = -0.26
 x0_um_27deg = -0.01
 # and in time
-t0_sc_27deg = unified_start( sla1_sc_27 ) + 11.2 
-t0_pb_27deg = unified_start( sla1_sp_27 ) + 4.15
+t0_sc_27deg = unified_start( sla1_sc_27 ) + 11.9 
+t0_sp_27deg = unified_start( sla1_sp_27 ) + 4.45
 t0_um_27deg = unified_start( sla1_um_27 ) + 9.0 
 
 # at 30 degree
 # in space
-x0_sc_30deg = -0.03
-x0_pb_30deg = -0.55
+x0_sc_30deg = -0.04
+x0_sp_30deg = -0.55
 x0_um_30deg = -0.01
 # and in time
-t0_sc_30deg = unified_start( sla1_sc_30 ) + 8.7 
-t0_pb_30deg = unified_start( sla1_sp_30 ) + 1.35
-t0_um_30deg = unified_start( sla1_um_30 ) + 8.3 
+t0_sc_30deg = unified_start( sla1_sc_30 ) + 9.4 
+t0_sp_30deg = unified_start( sla1_sp_30 ) + 1.95
+t0_um_30deg = unified_start( sla1_um_30 ) + 8.6 
 
 # Colors
 sla1_sp_color = '#000000'
--- Protein_tracking/plot.py
+++ Protein_tracking/plot.py
@@ -8,7 +8,7 @@ matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 
 def reference_plot_at_24deg( ax ) :
-    ax.plot( sla1_sp_24.t() - t0_pb_24deg , 100 * ( sla1_sp_24.coord()[ 0 ] - x0_pb_24deg ) ,label = 'Sla1-GFP in $S. pombe$ at $24\degree$C' , color = sla1_sp_color_24deg )
+    ax.plot( sla1_sp_24.t() - t0_sp_24deg , 100 * ( sla1_sp_24.coord()[ 0 ] - x0_sp_24deg ) ,label = 'Sla1-GFP in $S. pombe$ at $24\degree$C' , color = sla1_sp_color_24deg )
 
 fig = plt.figure( constrained_layout = True , figsize = ( 15 , 7 ) )
 
@@ -22,7 +22,7 @@ axes = fig.subplot_mosaic(
 # 18 degree
 
 myplot( axes["A"] , sla1_sc_18 , what = 'coord' , label = 'Sla1-GFP in $S. cerevisiae$' , x0 = x0_sc_18deg , t0 = t0_sc_18deg , col = sla1_sc_color , x_scale = 100 )
-myplot( axes["A"] , sla1_sp_18 , what = 'coord' , label = 'Sla1-GFP in $S. pombe$' , x0 = x0_pb_18deg, t0 = t0_pb_18deg, col = sla1_sp_color , x_scale = 100 )
+myplot( axes["A"] , sla1_sp_18 , what = 'coord' , label = 'Sla1-GFP in $S. pombe$' , x0 = x0_sp_18deg, t0 = t0_sp_18deg, col = sla1_sp_color , x_scale = 100 )
 myplot( axes["A"] , sla1_um_18 , what = 'coord' , label = 'Sla1-GFP in $U. maydis$' , x0 = x0_um_18deg , t0 = t0_um_18deg , col = sla1_um_color , x_scale = 100 )
 reference_plot_at_24deg( axes["A"] )
 
@@ -31,7 +31,7 @@ layout( axes[ "A"] , tlim[ "A"] , movlim["A"] , '$18\degree$C' , legend = True )
 # 21 degree
 
 myplot( axes["B"] , sla1_sc_21 , what = 'coord' , label = None, x0 = x0_sc_21deg , t0 = t0_sc_21deg , col = sla1_sc_color , x_scale = 100 )
-myplot( axes["B"] , sla1_sp_21 , what = 'coord' , label = None, x0 = x0_pb_21deg, t0 = t0_pb_21deg, col = sla1_sp_color , x_scale = 100 )
+myplot( axes["B"] , sla1_sp_21 , what = 'coord' , label = None, x0 = x0_sp_21deg, t0 = t0_sp_21deg, col = sla1_sp_color , x_scale = 100 )
 myplot( axes["B"] , sla1_um_21 , what = 'coord' , label = None, x0 = x0_um_21deg , t0 = t0_um_21deg , col = sla1_um_color , x_scale = 100 )
 reference_plot_at_24deg( axes["B"] )
 
@@ -40,7 +40,7 @@ layout( axes[ "B"] , tlim[ "B"] , movlim["B"] , '$21\degree$C' , yaxis_label = F
 # 24 degree
 
 myplot( axes["C"] , sla1_sc_24 , what = 'coord' , label = None, x0 = x0_sc_24deg , t0 = t0_sc_24deg , col = sla1_sc_color , x_scale = 100 )
-myplot( axes["C"] , sla1_sp_24 , what = 'coord' , label = None, x0 = x0_pb_24deg, t0 = t0_pb_24deg, col = sla1_sp_color , x_scale = 100 )
+myplot( axes["C"] , sla1_sp_24 , what = 'coord' , label = None, x0 = x0_sp_24deg, t0 = t0_sp_24deg, col = sla1_sp_color , x_scale = 100 )
 myplot( axes["C"] , sla1_um_24 , what = 'coord' , label = None, x0 = x0_um_24deg , t0 = t0_um_24deg , col = sla1_um_color , x_scale = 100 )
 
 layout( axes[ "C"] , tlim[ "C"] , movlim["C"] , '$24\degree$C' )
@@ -48,7 +48,7 @@ layout( axes[ "C"] , tlim[ "C"] , movlim["C"] , '$24\degree$C' )
 # 27 degree
 
 myplot( axes["D"] , sla1_sc_27 , what = 'coord' , label = None, x0 = x0_sc_27deg , t0 = t0_sc_27deg , col = sla1_sc_color , x_scale = 100 )
-myplot( axes["D"] , sla1_sp_27 , what = 'coord' , label = None, x0 = x0_pb_27deg, t0 = t0_pb_27deg, col = sla1_sp_color , x_scale = 100 )
+myplot( axes["D"] , sla1_sp_27 , what = 'coord' , label = None, x0 = x0_sp_27deg, t0 = t0_sp_27deg, col = sla1_sp_color , x_scale = 100 )
 myplot( axes["D"] , sla1_um_27 , what = 'coord' , label = None, x0 = x0_um_27deg , t0 = t0_um_27deg , col = sla1_um_color , x_scale = 100 )
 reference_plot_at_24deg( axes["D"] )
 
@@ -57,7 +57,7 @@ layout( axes[ "D"] , tlim[ "D"] , movlim["D"] , '$27\degree$C' , yaxis_label = F
 # 30 degree
 
 myplot( axes["E"] , sla1_sc_30 , what = 'coord' , label = None, x0 = x0_sc_30deg , t0 = t0_sc_30deg , col = sla1_sc_color , x_scale = 100 )
-myplot( axes["E"] , sla1_sp_30 , what = 'coord' , label = None, x0 = x0_pb_30deg, t0 = t0_pb_30deg, col = sla1_sp_color , x_scale = 100 )
+myplot( axes["E"] , sla1_sp_30 , what = 'coord' , label = None, x0 = x0_sp_30deg, t0 = t0_sp_30deg, col = sla1_sp_color , x_scale = 100 )
 myplot( axes["E"] , sla1_um_30 , what = 'coord' , label = None, x0 = x0_um_30deg , t0 = t0_um_30deg , col = sla1_um_color , x_scale = 100 )
 reference_plot_at_24deg( axes["E"] )
 
