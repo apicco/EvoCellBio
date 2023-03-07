f3862cf07c876e43bacb747621048773ba746bcc
Andrea Picco <apicco@gmail.com>
2023-03-07 11:43:11 +0100

    add the coat movement reaction norm

--- Protein_tracking/funs.py
+++ Protein_tracking/funs.py
@@ -13,32 +13,13 @@ def set_x0( t , range = [ -5 , 0 ] ) :
     x0 = np.nanmedian( tt.coord()[ 0 ] )
     return x0
 
-def layout( ax , tlim , movlim , title , yaxis_label = True , legend = False ) :
-    
-    xl = [ i for i in range( 0 , int( tlim[ 0 ] ) , -5 ) ]
-    xr = [ i for i in range( 0 , int( tlim[ 1 ] ) , 5 ) ]
-    ax.set_xticks( xl[::-1] + xr[1:] )
-    
-    #plt.subplot( 211 )
-    ax.set_ylim( movlim )
-    ax.set_xlim( tlim )
-    ax.tick_params( labelsize = 10 )
-    ax.set_xlabel( 'Time (s)' , fontsize = 13 )
-    if yaxis_label : ax.set_ylabel( 'Inward movement (nm)' , fontsize = 13 )
-    ax.grid()
-    if legend : ax.legend( loc = 'upper left' , fontsize = 10 )
-    ax.set_title( title , fontsize = 18 )
-	
-def layout_velocities( ax , title , ylabel = True , legend = False ) :
+def coat_movement( t , scale , n = 5 ) :
 
-    ax.set_xlim( 16 , 32 )
-    ax.set_xticks( [ 18,21,24,27,30] )
-    ax.tick_params( labelsize = 10 )
-    ax.set_xlabel( "T ($^{o}C$)" , fontsize = 13 )
-    if ylabel : ax.set_ylabel( "$v$ ($nm/s$)" , fontsize = 13 )
-    ax.grid()
-    if legend : ax.legend( loc = 'upper left' , fontsize = 10 )
-    ax.set_title( title , fontsize = 18 )
+    l = len( t ) - 1
+    
+    h = np.mean( t.coord()[ 0 ][ l - n : l ] * scale )
+    h_err = np.sqrt( np.sum( t.coord_err()[ 0 ][ l - n : l ] ** 2 ) ) * scale / n
+    return [ h , h_err ]
 
 def velocity( t , range , scale , t0 = 0 ) :
 
@@ -67,3 +48,33 @@ def velocity( t , range , scale , t0 = 0 ) :
     e = np.sqrt( cov[ 0 , 0 ] ) / ( 2 * np.sqrt( p[ 0 ] ) )
 
     return [ p[ 0 ] * scale , e * scale ]
+
+# LAYOUTS
+def layout( ax , tlim , movlim , title , yaxis_label = True , legend = False ) :
+    
+    xl = [ i for i in range( 0 , int( tlim[ 0 ] ) , -5 ) ]
+    xr = [ i for i in range( 0 , int( tlim[ 1 ] ) , 5 ) ]
+    ax.set_xticks( xl[::-1] + xr[1:] )
+    
+    #plt.subplot( 211 )
+    ax.set_ylim( movlim )
+    ax.set_xlim( tlim )
+    ax.tick_params( labelsize = 10 )
+    ax.set_xlabel( 'Time (s)' , fontsize = 13 )
+    if yaxis_label : ax.set_ylabel( 'Inward movement (nm)' , fontsize = 13 )
+    ax.grid()
+    if legend : ax.legend( loc = 'upper left' , fontsize = 10 )
+    ax.set_title( title , fontsize = 18 )
+	
+def layout_rn( ax , title , ylabel , legend = False ) :
+
+    ax.set_xlim( 16 , 32 )
+    ax.set_xticks( [ 18,21,24,27,30] )
+    ax.tick_params( labelsize = 10 )
+    ax.set_xlabel( "T ($^{o}C$)" , fontsize = 13 )
+    if ylabel : ax.set_ylabel( ylabel , fontsize = 13 )
+    ax.grid()
+    if legend : ax.legend( loc = 'upper left' , fontsize = 10 )
+    ax.set_title( title , fontsize = 18 )
+
+
--- Protein_tracking/reaction_norm.py
+++ Protein_tracking/reaction_norm.py
@@ -1,6 +1,6 @@
 from data import *
 from params import *
-from funs import velocity , layout_velocities
+from funs import velocity , coat_movement, layout_rn
 from trajplot.plotfuns import myplot , plot_raw
 
 import matplotlib as mpl
@@ -8,6 +8,8 @@ mpl.use('Agg')
 cmap = mpl.colormaps['plasma'].resampled( 6 )
 from matplotlib import pyplot as plt
 
+# compute velocitis
+# range
 x_fit = [ 0.3 , 0.9 ]
 # S. cerevisiae
 vsla1_sc_18 = velocity( sla1_sc_18 , x_fit , scale = 100 ) 
@@ -24,6 +26,8 @@ vsla1_sp_27 = velocity( sla1_sp_27 , x_fit , scale = 100 )
 vsla1_sp_30 = velocity( sla1_sp_30 , x_fit , scale = 100 ) 
 
 # U. maydis
+# U. maydis has a shorter invagination, the range where
+# to compute the fit ought to be adapted case by case
 x_fit = [ 0.3 , 0.9 ]
 vsla1_um_18 = velocity( sla1_um_18 , x_fit , scale = 100 ) 
 x_fit = [ 0.2 , 0.6 ]
@@ -35,13 +39,36 @@ vsla1_um_27 = velocity( sla1_um_27 , x_fit , scale = 100 )
 x_fit = [ 0.1 , 0.4 ]
 vsla1_um_30 = velocity( sla1_um_30 , x_fit , scale = 100 ) 
 
-fig = plt.figure( constrained_layout = True , figsize = ( 5 , 3 ) )
+# compute coat movement heights
+# S. cerevisiae
+hsla1_sc_18 = coat_movement( sla1_sc_18 , scale = 100 )
+hsla1_sc_21 = coat_movement( sla1_sc_21 , scale = 100 )
+hsla1_sc_24 = coat_movement( sla1_sc_24 , scale = 100 )
+hsla1_sc_27 = coat_movement( sla1_sc_27 , scale = 100 )
+hsla1_sc_30 = coat_movement( sla1_sc_30 , scale = 100 )
+
+# S. pombe
+hsla1_sp_18 = coat_movement( sla1_sp_18 , scale = 100 )
+hsla1_sp_21 = coat_movement( sla1_sp_21 , scale = 100 )
+hsla1_sp_24 = coat_movement( sla1_sp_24 , scale = 100 )
+hsla1_sp_27 = coat_movement( sla1_sp_27 , scale = 100 )
+hsla1_sp_30 = coat_movement( sla1_sp_30 , scale = 100 )
+
+# U. maydis
+hsla1_um_18 = coat_movement( sla1_um_18 , scale = 100 )
+hsla1_um_21 = coat_movement( sla1_um_21 , scale = 100 )
+hsla1_um_24 = coat_movement( sla1_um_24 , scale = 100 )
+hsla1_um_27 = coat_movement( sla1_um_27 , scale = 100 )
+hsla1_um_30 = coat_movement( sla1_um_30 , scale = 100 )
+
+fig = plt.figure( constrained_layout = True , figsize = ( 8 , 3 ) )
 axes = fig.subplot_mosaic( 
     """
-    A
+    AB
     """ , 
-    sharey = True )
+    sharey = False )
 
+# Velocity reaction norm
 # S. cerevisiae
 axes["A"].errorbar( 18 , vsla1_sc_18[ 0 ] , vsla1_sc_18[ 1 ] , label = '$S. cerevisiae$' , color = sla1_sc_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
 axes["A"].errorbar( 21 , vsla1_sc_21[ 0 ] , vsla1_sc_21[ 1 ] , color = sla1_sc_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
@@ -50,6 +77,7 @@ axes["A"].errorbar( 27 , vsla1_sc_27[ 0 ] , vsla1_sc_27[ 1 ] , color = sla1_sc_c
 axes["A"].errorbar( 30 , vsla1_sc_30[ 0 ] , vsla1_sc_30[ 1 ] , color = sla1_sc_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
 axes["A"].plot( [ 18,21,24,27,30 ] , [ vsla1_sc_18[0] , vsla1_sc_21[0] , vsla1_sc_24[0] , vsla1_sc_27[0] , vsla1_sc_30[0] ] , color = "#CDCDCD" , linestyle = '--' , linewidth = 2 ) 
 
+# S. pombe
 axes["A"].errorbar( 18 , vsla1_sp_18[ 0 ] , vsla1_sp_18[ 1 ] , label = '$S. pombe$' , color = sla1_sp_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
 axes["A"].errorbar( 21 , vsla1_sp_21[ 0 ] , vsla1_sp_21[ 1 ] , color = sla1_sp_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
 axes["A"].errorbar( 24 , vsla1_sp_24[ 0 ] , vsla1_sp_24[ 1 ] , color = sla1_sp_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
@@ -57,13 +85,40 @@ axes["A"].errorbar( 27 , vsla1_sp_27[ 0 ] , vsla1_sp_27[ 1 ] , color = sla1_sp_c
 axes["A"].errorbar( 30 , vsla1_sp_30[ 0 ] , vsla1_sp_30[ 1 ] , color = sla1_sp_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
 axes["A"].plot( [ 18,21,24,27,30 ] , [ vsla1_sp_18[0] , vsla1_sp_21[0] , vsla1_sp_24[0] , vsla1_sp_27[0] , vsla1_sp_30[0] ] , color = "#CDCDCD" , linestyle = '--' , linewidth = 2 ) 
 
+# U. maydis
 axes["A"].errorbar( 18 , vsla1_um_18[ 0 ] , vsla1_um_18[ 1 ] , label = '$U.maydis$' , color = sla1_um_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
 axes["A"].errorbar( 21 , vsla1_um_21[ 0 ] , vsla1_um_21[ 1 ] , color = sla1_um_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
 axes["A"].errorbar( 24 , vsla1_um_24[ 0 ] , vsla1_um_24[ 1 ] , color = sla1_um_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
 axes["A"].errorbar( 27 , vsla1_um_27[ 0 ] , vsla1_um_27[ 1 ] , color = sla1_um_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
 axes["A"].errorbar( 30 , vsla1_um_30[ 0 ] , vsla1_um_30[ 1 ] , color = sla1_um_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
 axes["A"].plot( [ 18,21,24,27,30 ] , [ vsla1_um_18[0] , vsla1_um_21[0] , vsla1_um_24[0] , vsla1_um_27[0] , vsla1_um_30[0] ] , color = "#CDCDCD" , linestyle = '--' , linewidth = 2 ) 
-layout_velocities( axes["A"] , "$v$ reaction norm" , legend = True )
+layout_rn( axes["A"] , "$v$ reaction norm" , "$v$ ($nm/s$)" , legend = True )
+
+# Coat dynamics reaction norm
+# S. cerevisiae
+axes["B"].errorbar( 18 , hsla1_sc_18[ 0 ] , hsla1_sc_18[ 1 ] , label = '$S. cerevisiae$' , color = sla1_sc_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
+axes["B"].errorbar( 21 , hsla1_sc_21[ 0 ] , hsla1_sc_21[ 1 ] , color = sla1_sc_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
+axes["B"].errorbar( 24 , hsla1_sc_24[ 0 ] , hsla1_sc_24[ 1 ] , color = sla1_sc_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
+axes["B"].errorbar( 27 , hsla1_sc_27[ 0 ] , hsla1_sc_27[ 1 ] , color = sla1_sc_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
+axes["B"].errorbar( 30 , hsla1_sc_30[ 0 ] , hsla1_sc_30[ 1 ] , color = sla1_sc_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
+axes["B"].plot( [ 18,21,24,27,30 ] , [ hsla1_sc_18[0] , hsla1_sc_21[0] , hsla1_sc_24[0] , hsla1_sc_27[0] , hsla1_sc_30[0] ] , color = "#CDCDCD" , linestyle = '--' , linewidth = 2 ) 
+
+# S. pombe
+axes["B"].errorbar( 18 , hsla1_sp_18[ 0 ] , hsla1_sp_18[ 1 ] , label = '$S. pombe$' , color = sla1_sp_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
+axes["B"].errorbar( 21 , hsla1_sp_21[ 0 ] , hsla1_sp_21[ 1 ] , color = sla1_sp_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
+axes["B"].errorbar( 24 , hsla1_sp_24[ 0 ] , hsla1_sp_24[ 1 ] , color = sla1_sp_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
+axes["B"].errorbar( 27 , hsla1_sp_27[ 0 ] , hsla1_sp_27[ 1 ] , color = sla1_sp_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
+axes["B"].errorbar( 30 , hsla1_sp_30[ 0 ] , hsla1_sp_30[ 1 ] , color = sla1_sp_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
+axes["B"].plot( [ 18,21,24,27,30 ] , [ hsla1_sp_18[0] , hsla1_sp_21[0] , hsla1_sp_24[0] , hsla1_sp_27[0] , hsla1_sp_30[0] ] , color = "#CDCDCD" , linestyle = '--' , linewidth = 2 ) 
+
+# U. maydis
+axes["B"].errorbar( 18 , hsla1_um_18[ 0 ] , hsla1_um_18[ 1 ] , label = '$U.maydis$' , color = sla1_um_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
+axes["B"].errorbar( 21 , hsla1_um_21[ 0 ] , hsla1_um_21[ 1 ] , color = sla1_um_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
+axes["B"].errorbar( 24 , hsla1_um_24[ 0 ] , hsla1_um_24[ 1 ] , color = sla1_um_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
+axes["B"].errorbar( 27 , hsla1_um_27[ 0 ] , hsla1_um_27[ 1 ] , color = sla1_um_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
+axes["B"].errorbar( 30 , hsla1_um_30[ 0 ] , hsla1_um_30[ 1 ] , color = sla1_um_color , marker = 'o' , markersize = 7 , elinewidth = 2.0 , capsize = 8 , capthick = 2.0 )
+axes["B"].plot( [ 18,21,24,27,30 ] , [ hsla1_um_18[0] , hsla1_um_21[0] , hsla1_um_24[0] , hsla1_um_27[0] , hsla1_um_30[0] ] , color = "#CDCDCD" , linestyle = '--' , linewidth = 2 ) 
+layout_rn( axes["B"] , "movement reaction norm" , "coat movement ($nm$)" , legend = False )
 
 fig.savefig( "reaction_norm.pdf" )
 
