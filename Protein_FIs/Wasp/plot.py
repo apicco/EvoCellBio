77d658e3932cc8bce58d959f0add9e533bddeaca
apicco <andrea.picco@unige.ch>
2023-03-08 15:00:27 +0100

    rename plot at 24 deg for fig 4

--- Protein_FIs/Wasp/plot.py
+++ Protein_FIs/Wasp/plot.py
@@ -1,54 +1,27 @@
-from trajalign.average import load_directory
+import sys
+
+# append parent directory for global plot properties
+sys.path.append( '../../' )
+from Global.layouts import layout
+
+from params import *
 from funs import *
 from data import *
+
 import matplotlib
 
-#--- DEFINE THE XLIM
-
-xlim_sc = [ -50 , 50 ]
-xlim_sp = [ -50 , 50 ]
-xlim_um = [ -50 , 50 ]
-
-xlim_sc_spwasp = [ -50 , 50 ]
-xlim_sc_sla1del= [ -237 , 75 ]
-xlim_sc_sla1del_spwasp= [ -30 , 70 ]
-
-d_xlim_sc = xlim_sc[ 1 ] - xlim_sc[ 0 ]
-d_xlim_sp = xlim_sp[ 1 ] - xlim_sp[ 0 ]
-d_xlim_um = xlim_um[ 1 ] - xlim_um[ 0 ]
-d_wt = d_xlim_sc + d_xlim_sp + d_xlim_um 
-wt_figsize_x = 8
-
-d_spwasp = xlim_sc_spwasp[ 1 ] - xlim_sc_spwasp[ 0 ]
-d_sla1del = xlim_sc_sla1del[ 1 ] - xlim_sc_sla1del[ 0 ]
-d_sla1del_spwasp = xlim_sc_sla1del_spwasp[ 1 ] - xlim_sc_sla1del_spwasp[ 0 ]
-d_mut = d_spwasp + d_sla1del + d_sla1del_spwasp
-mut_figsize_x = wt_figsize_x * ( d_mut ) / ( d_wt )
-
-# figure is
-# l1 * f + [ Y + ( x2 / x1 ) * Y  + ( x3 / x1 ) * Y ] * ( 1 + 2 * d1 )  + r1 * f = f
-# thus
-# Y * ( 1 + x2 / x1 + x3 / x1 ) = ( 1 - l1 - r1 ) * f / ( 1 + 2 * d1 ) 
-# thus
-# Y = ( 1 - l1 - r1 ) * f / [ ( 1 + 2 * d1 ) * ( 1 + x2 / x1 + x3 / x1 ) ]
-
-# where l1 = 0.125 ,  r1 = 0.9 , and d1 = 0.1 
-
-#------------------
-# PLOT
-#------------------
-
-fig = plt.figure( constrained_layout = True , figsize = ( 15 , 7 ) )
+fig = plt.figure( constrained_layout = True , figsize = ( 8 , 4 ) )
 
 axes = fig.subplot_mosaic(
         """
-        ABC..
+        ..ABC
         DEEEF
         """ ,
         sharey = True )
 
 # WT
 plot_all( axes['A'] , g_4021 , r_4021 , g_label = '$Sc_{Wasp}$' , r_label = 'Fim1' , alpha = 0.1 , xlim = xlim_sc )
+layout( axes[ 'A' ] , tlim[ 'A' ] , flim[ 'A' ] , '$S. cerevisiae' , legend = True )
 plot_all( axes['B'] , g_0026 , r_0026 , g_label = '$Sp_{Wasp}$' , r_label = 'Fim1' , alpha = 0.1 , xlim = xlim_sp )
 plot_all( axes['C'] , g_0024 , r_0024 , g_label = '$Um_{Wasp}$' , r_label = 'Fim1' , alpha = 0.1 , xlim = xlim_um )
 # mutations
--- Protein_tracking/plot_24deg.py
+++ Protein_tracking/plot_24deg.py
@@ -23,7 +23,7 @@ myplot( axes["A"] , sla1_sc_24 , what = 'coord' , label = '$S. cerevisiae$' , co
 myplot( axes["A"] , sla1_sp_24 , what = 'coord' , label = '$S. pombe$' , col = sla1_sp_color , x_scale = 100 , add_CI = False )
 myplot( axes["A"] , sla1_um_24 , what = 'coord' , label = '$U. maydis$' , col = sla1_um_color , x_scale = 100 , add_CI = False )
 # use layout range for pannel at 24 degree in figure 5
-layout( axes[ "A"] , tlim[ "C"] , movlim["C"] , 'Coat dynamics at $24\degree$C' , legend = True , legend_title = "Sla1-GFP" , xaxis_label = False )
+layout( axes[ "A"] , tlim[ "C"] , movlim["C"] , 'Coat dynamics' , legend = True , legend_title = "Sla1-GFP" , xaxis_label = False )
 
 myplot( axes["B"] , sla1_sc_24 , what = 'f' , label = '$S. cerevisiae$' , col = sla1_sc_color , add_CI = False )
 myplot( axes["B"] , sla1_sp_24 , what = 'f' , label = '$S. pombe$' , col = sla1_sp_color , add_CI = False )
