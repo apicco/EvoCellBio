7442c986c1bc862ce7bfd4d710f12d0c6bfe7751
apicco <andrea.picco@unige.ch>
2023-03-09 14:41:17 +0100

    finalise the layout of the wasp plots and split them in two to counter the mosaic subplot bug

---
+++ Protein_FIs/Wasp/params.py
@@ -0,0 +1,19 @@
+from trajalign.average import load_directory
+from funs import *
+from data import *
+import matplotlib
+
+#----------------------------
+# Define the plot parameters
+#----------------------------
+
+tlim = {    
+        "A" : ( -42 , 40 ) ,    
+        "B" : ( -60 , 54 ) , 
+        "C" : ( -42 , 40 ) ,
+        "D" : ( -42 , 40 ) ,
+        "E" : ( -228 , 50 ) ,
+        "F" : ( -33 , 80 ) 
+        }
+
+flim = ( -0.1 , 2.1 )
--- Protein_FIs/Wasp/plot.py
+++ Protein_FIs/Wasp/plot.py
@@ -8,13 +8,12 @@ from params import *
 from funs import *
 from data import *
 
-import matplotlib
+ys = 4 # vertical size
 
-fig = plt.figure( figsize = ( 8 , 6 ) )#, layout = 'constrained' )
-fig.set_tight_layout(True)
-axes = fig.subplot_mosaic([
-    3*['A'] + 4*['B'] + 3*['C'] + 6*['.'] ,
-    3*['D'] + 9*['E'] + 4*['F']
+fig1 = plt.figure( figsize = ( 8 , ys ) )#, layout = 'constrained' )
+fig1.set_tight_layout(True)
+axes = fig1.subplot_mosaic([
+    3*['A'] + 4*['B'] + 3*['C'] + 6*['.']
     ], sharey = True )
 
 # WT
@@ -24,6 +23,15 @@ plot_all( axes['B'] , g_4021 , r_4021 , g_label = '$Sc_{Wasp}$' , r_label = 'Fim
 layout( axes[ 'B' ] , tlim[ 'B' ] , flim , '$S.cerevisiae$' , legend = False , ylabel = "Fluor. int .($a.u.$)" , yaxis_label = False , xtick_interval = 25 )
 plot_all( axes['C'] , g_0026 , r_0026 , g_label = '$Sp_{Wasp}$' , r_label = 'Fim1' , alpha = 0.1 , xlim = tlim[ 'C' ] )
 layout( axes[ 'C' ] , tlim[ 'C' ] , flim , '$S.pombe$' , legend = False , ylabel = "Fluor. int .($a.u.$)" , yaxis_label = False , xtick_interval = 25 )
+
+plt.savefig( 'WT_Wasp.pdf' )
+
+fig2 = plt.figure( figsize = ( 8 , ys ) )#, layout = 'constrained' )
+fig2.set_tight_layout(True)
+axes = fig2.subplot_mosaic([
+    3*['D'] + 9*['E'] + 4*['F']
+    ], sharey = True )
+
 # mutations
 plot_all( axes['D'] , g_4390 , r_4390 , g_label = '' , r_label = 'Fim1' , alpha = 0.1 , xlim = tlim[ 'D' ] )
 layout( axes[ 'D' ] , tlim[ 'D' ] , flim , '$Wasp_{Sp}$' , legend = False , ylabel = "Fluor. int .($a.u.$)" , xtick_interval = 25 )
@@ -32,5 +40,5 @@ layout( axes[ 'E' ] , tlim[ 'E' ] , flim , '$Wasp_{Sc}$, $sla1\Delta$' , legend 
 plot_all( axes['F'] , g_4794 , r_4794 , g_label = '' , r_label = 'Fim1' , alpha = 0.1 , xlim = tlim[ 'F' ] )
 layout( axes[ 'F' ] , tlim[ 'F' ] , flim , '$Wasp_{Sp}$, $sla1\Delta$' , legend = False , ylabel = "Fluor. int .($a.u.$)" , yaxis_label = False , xtick_interval = 25 )
 
-plt.savefig( 'plot.pdf' )
+plt.savefig( 'Swap_Wasp.pdf' )
 
