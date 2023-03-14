a6cacda811ede652a77aad9df7abe9ea30044cb9
apicco <andrea.picco@unige.ch>
2023-03-14 13:43:16 +0100

    unify color access

---
+++ Global/colors.py
@@ -0,0 +1,20 @@
+# colors
+
+color_Ede1 = '#F9DDBD'
+color_Ede1_Ucp8 =  color_Ede1
+color_Sla1 = '#EC9939'
+color_Pan1 = '#F2BB7B'
+color_Wasp = '#1191D1'
+color_Myo1 = '#88C8E8'
+color_Myo3 = color_Myo1
+color_Myo5 = color_Myo1
+color_Rvs = '#147C33'
+color_Arc18 = '#DD5C6A'
+color_Fim1 = '#CC0A20'
+
+# colors for the invagination movements
+sla1_sp_color = '#08A045'
+sla1_sc_color = "#30011E"
+#sla1_um_color = '#00487C'
+sla1_um_color = '#82A6B1'
+sla1_sp_color_24deg = '#CDCDCD'
