45a49e0ba9e3c5c9eed84bf7ed86f113438de223
apicco <andrea.picco@unige.ch>
2023-05-12 17:10:40 +0200

    remove guide lines to eyeball the range where to estimate the reaction norm

--- Protein_tracking/plot_by_species.py
+++ Protein_tracking/plot_by_species.py
@@ -20,15 +20,15 @@ axes = fig.subplot_mosaic(
     """ , 
     sharey = True )
 # S. cerevisiae
-rg = [ 15 , 55 ]
 
 myplot( axes["A"] , sla1_sc_18 , what = 'coord' , label = '$18\degree$C' , col = cmap(0) , x_scale = 100 , add_CI = False )
 myplot( axes["A"] , sla1_sc_21 , what = 'coord' , label = '$21\degree$C' , col = cmap(1) , x_scale = 100 , add_CI = False )
 myplot( axes["A"] , sla1_sc_24 , what = 'coord' , label = '$24\degree$C' , col = cmap(2) , x_scale = 100 , add_CI = False )
 myplot( axes["A"] , sla1_sc_27 , what = 'coord' , label = '$27\degree$C' , col = cmap(3) , x_scale = 100 , add_CI = False )
 myplot( axes["A"] , sla1_sc_30 , what = 'coord' , label = '$30\degree$C' , col = cmap(4) , x_scale = 100 , add_CI = False )
-axes[ "A" ].plot( [ i for i in range(-10 , 20 ) ] , [ rg[ 0 ] ] * 30 , 'k--' )
-axes[ "A" ].plot( [ i for i in range(-10 , 20 ) ] , [ rg[ 1 ] ] * 30 , 'k--' )
+#rg = [ 15 , 55 ]
+#axes[ "A" ].plot( [ i for i in range(-10 , 20 ) ] , [ rg[ 0 ] ] * 30 , 'k--' )
+#axes[ "A" ].plot( [ i for i in range(-10 , 20 ) ] , [ rg[ 1 ] ] * 30 , 'k--' )
 layout( axes[ "A"] , species_tlim[ "A"] , movlim["B"] , '$S. cerevisiae$' , legend = True , legend_title = "Sla1-GFP" )
 
 # S. pombe
@@ -38,8 +38,8 @@ myplot( axes["B"] , sla1_sp_21 , what = 'coord' , label = '$21\degree$C' , col =
 myplot( axes["B"] , sla1_sp_24 , what = 'coord' , label = '$24\degree$C' , col = cmap(2) , x_scale = 100 , add_CI = False )
 myplot( axes["B"] , sla1_sp_27 , what = 'coord' , label = '$27\degree$C' , col = cmap(3) , x_scale = 100 , add_CI = False )
 myplot( axes["B"] , sla1_sp_30 , what = 'coord' , label = '$30\degree$C' , col = cmap(4) , x_scale = 100 , add_CI = False )
-axes[ "B" ].plot( [ i for i in range(-10 , 20 ) ] , [ rg[ 0 ] ] * 30 , 'k--' )
-axes[ "B" ].plot( [ i for i in range(-10 , 20 ) ] , [ rg[ 1 ] ] * 30 , 'k--' )
+#axes[ "B" ].plot( [ i for i in range(-10 , 20 ) ] , [ rg[ 0 ] ] * 30 , 'k--' )
+#axes[ "B" ].plot( [ i for i in range(-10 , 20 ) ] , [ rg[ 1 ] ] * 30 , 'k--' )
 
 layout( axes[ "B"] , species_tlim["B"] , movlim["B"] , '$S. pombe$' , yaxis_label = False )
 
@@ -51,8 +51,8 @@ myplot( axes["C"] , sla1_um_21 , what = 'coord' , label = '$21\degree$C' , col =
 myplot( axes["C"] , sla1_um_24 , what = 'coord' , label = '$24\degree$C' , col = cmap(2) , x_scale = 100 , add_CI = False )
 myplot( axes["C"] , sla1_um_27 , what = 'coord' , label = '$27\degree$C' , col = cmap(3) , x_scale = 100 , add_CI = False )
 myplot( axes["C"] , sla1_um_30 , what = 'coord' , label = '$30\degree$C' , col = cmap(4) , x_scale = 100 , add_CI = False )
-axes[ "C" ].plot( [ i for i in range(-10 , 20 ) ] , [ rg[ 0 ] ] * 30 , 'k--' )
-axes[ "C" ].plot( [ i for i in range(-10 , 20 ) ] , [ rg[ 1 ] ] * 30 , 'k--' )
+#axes[ "C" ].plot( [ i for i in range(-10 , 20 ) ] , [ rg[ 0 ] ] * 30 , 'k--' )
+#axes[ "C" ].plot( [ i for i in range(-10 , 20 ) ] , [ rg[ 1 ] ] * 30 , 'k--' )
 
 layout( axes[ "C"] , species_tlim[ "C"] , movlim["C"] , '$U. maydis$' , yaxis_label = False )
 
