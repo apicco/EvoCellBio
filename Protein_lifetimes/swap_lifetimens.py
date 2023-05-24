a721905e68824ffa18c7c2af0958a92b8de8ee64
apicco <andrea.picco@unige.ch>
2023-05-24 15:34:07 +0200

    shirnks a notc the bars and update nomenclature

--- Protein_lifetimes/swap_lifetimens.py
+++ Protein_lifetimes/swap_lifetimens.py
@@ -12,7 +12,7 @@ from funs import *
 
 # locally redefine color_Wasp (consider if redefine globally)
 color_Wasp = '#29A300'
-fig , ax = plt.subplots( 2 , 1 , figsize = ( 8 , 4 ) , sharex = 'all' , gridspec_kw = { 'height_ratios' : [3.5, 1] } )
+fig , ax = plt.subplots( 2 , 1 , figsize = ( 8 , 3.8 ) , sharex = 'all' , gridspec_kw = { 'height_ratios' : [3.5, 1] } )
 
 sc = ax[ 0 ]
 sp = ax[ 1 ]
@@ -27,12 +27,12 @@ lt( sc , sla1del_Las17_sc , -6 , 2 , dt = 1.24 , shift = shift_sc , col = color_
 lt( sc , sla1del_spWasp_sc , -9 , 2 , dt = 1.24 , shift = shift_sc , col = color_Wasp )
 
 layout_swap( sc , '$S. cerevisiae$' )
-sc.set_title( 'Wasp lifetime' , fontsize = 18 )
+sc.set_title( 'WASP lifetime' , fontsize = 18 )
 sc.set_yticklabels( [ 
-    "ScWasp" ,
-    "SpWasp" ,
-    "$sla1\Delta$ ScWasp" ,
-    "$sla1\Delta$ SpWasp"
+    "[Sc]WASP" ,
+    "[Sp]WASP" ,
+    "$sla1\Delta$ [Sc]WASP" ,
+    "$sla1\Delta$ [Sp]WASP"
 ] , fontsize = 13 )
 
 # invagination start
@@ -43,7 +43,7 @@ lt( sp , Wasp_sp , -0 , 2 , dt = 1.2 , shift = shift_sp , col = color_Wasp )
 
 layout_swap( sp , '$S. pombe$' , is_sc = False )
 sp.set_yticklabels( [ 
-                     "SpWasp"
+                     "[Sp]WASP"
                      ] , fontsize = 13 )
 
 plt.xlabel( 'Time (s)' , fontsize = 13 )
