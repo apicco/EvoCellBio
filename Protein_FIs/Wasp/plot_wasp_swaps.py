e8280e85f682be89449c2ba8fe974edd0de0e1e7
apicco <andrea.picco@unige.ch>
2023-05-25 13:17:50 +0200

    adjust title

--- Protein_FIs/Wasp/plot_wasp_swaps.py
+++ Protein_FIs/Wasp/plot_wasp_swaps.py
@@ -9,8 +9,9 @@ from funs import *
 from data import *
 
 ys = 3 # vertical size
+xs = 8 # horizontal size, constrained by page layout in affinity designer
 
-fig1 = plt.figure( figsize = ( 8 , ys ) )#, layout = 'constrained' )
+fig1 = plt.figure( figsize = ( xs , ys ) )#, layout = 'constrained' )
 fig1.set_tight_layout(True)
 axes = fig1.subplot_mosaic([
     3*['A'] + 4*['B'] + 3*['C'] + 6*['.']
@@ -26,19 +27,19 @@ layout( axes[ 'C' ] , tlim[ 'C' ] , flim , '$S.pombe$' , legend = False , ylabel
 
 plt.savefig( 'WT_Wasp.pdf' )
 
-fig2 = plt.figure( figsize = ( 8 , ys ) )#, layout = 'constrained' )
+fig2 = plt.figure( figsize = ( xs , ys ) )#, layout = 'constrained' )
 fig2.set_tight_layout(True)
 axes = fig2.subplot_mosaic([
     3*['D'] + 9*['E'] + 4*['F']
     ], sharey = True )
 
 # mutations
-plot_all( axes['D'] , g_4390 , r_4390 , g_label = 'Wasp' , r_label = 'Fim1' , alpha = 0.1 , xlim = tlim[ 'D' ] )
-layout( axes[ 'D' ] , tlim[ 'D' ] , flim , 'SpWasp' , legend = False , ylabel = "Fluor. int .($a.u.$)" , xtick_interval = 25 )
-plot_all( axes['E'] , g_4801 , r_4801 , g_label = 'Wasp' , r_label = 'Fim1' , alpha = 0.1 , xlim = tlim[ 'E' ] )
-layout( axes[ 'E' ] , tlim[ 'E' ] , flim , 'ScWasp, $sla1\Delta$' , legend = True , ylabel = "Fluor. int .($a.u.$)" , yaxis_label = False , xtick_interval = 50 )
-plot_all( axes['F'] , g_4794 , r_4794 , g_label = 'Wasp' , r_label = 'Fim1' , alpha = 0.1 , xlim = tlim[ 'F' ] )
-layout( axes[ 'F' ] , tlim[ 'F' ] , flim , 'SpWasp, $sla1\Delta$' , legend = False , ylabel = "Fluor. int .($a.u.$)" , yaxis_label = False , xtick_interval = 25 )
+plot_all( axes['D'] , g_4390 , r_4390 , g_label = 'WASP' , r_label = 'Fim1' , alpha = 0.1 , xlim = tlim[ 'D' ] )
+layout( axes[ 'D' ] , tlim[ 'D' ] , flim , '[Sp]WASP' , legend = False , ylabel = "Fluor. int .($a.u.$)" , xtick_interval = 25 )
+plot_all( axes['E'] , g_4801 , r_4801 , g_label = 'WASP' , r_label = 'Fim1' , alpha = 0.1 , xlim = tlim[ 'E' ] )
+layout( axes[ 'E' ] , tlim[ 'E' ] , flim , '[Sc]WASP, $sla1\Delta$' , legend = True , ylabel = "Fluor. int .($a.u.$)" , yaxis_label = False , xtick_interval = 50 )
+plot_all( axes['F'] , g_4794 , r_4794 , g_label = 'WASP' , r_label = 'Fim1' , alpha = 0.1 , xlim = tlim[ 'F' ] )
+layout( axes[ 'F' ] , tlim[ 'F' ] , flim , '[Sp]WASP, $sla1\Delta$ ' , legend = False , ylabel = "Fluor. int .($a.u.$)" , yaxis_label = False , xtick_interval = 25 )
 
 plt.savefig( 'Swap_Wasp.pdf' )
 
