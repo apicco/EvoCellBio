a968027bd06c3462a9ddcd49f5077372ef8aff6c
Andrea Picco <apicco@gmail.com>
2023-06-06 11:55:15 +0200

    add new Sac6 data

--- Protein_tracking/Data/Fimbrin/24_degree/Sc/fim1.py
+++ Protein_tracking/Data/Fimbrin/24_degree/Sc/fim1.py
@@ -6,22 +6,20 @@ trajectory_list = load_directory(
 		path = 'fim1_raw_trajectories' , 
 		pattern = '.data' ,
 		comment_char = '%' , 
-		dt = 0.0937 , 
+		dt = 0.104 , 
 		t_unit = 's' , 
 		coord_unit = 'pxl' , 
 		frames = 0 , 
 		coord = ( 1 , 2 ) , 
 		f = 3 , 
 		protein = 'Sac6-GFP' , 
-		date = '29/08/11' , 
+		date = '01/06/23' , 
 		notes = 'Sac6 average trajectory')
 
 best_median , worst_median , aligned_trajectories_median =\
 		average_trajectories( trajectory_list , max_frame = 500 , 
 				output_file = 'fim1' , median = True , fimax = True ) 
 
-import numpy as np
 s = Traj()
 s.load( 'fim1.txt' )
-s.rotate( np.pi )
 s.save( 'fim1_sc_24deg.txt' )
