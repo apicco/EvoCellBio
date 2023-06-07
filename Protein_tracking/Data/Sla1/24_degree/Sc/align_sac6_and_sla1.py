567d8773c671be76dc2a4078a30c8e2419f46ff8
apicco <andrea.picco@unige.ch>
2023-06-07 16:43:03 +0200

    compute alignment

--- Protein_tracking/Data/Sla1/24_degree/Sc/align_sac6_and_sla1.py
+++ Protein_tracking/Data/Sla1/24_degree/Sc/align_sac6_and_sla1.py
@@ -7,14 +7,14 @@ sla1_trajectories = load_directory(
 		path = path_raw_DW_trajectories ,
 		pattern = '.W2data.txt$' ,
 		comment_char = '%' , 
-		dt = 0.2715 , 
+		dt = 0.259 , 
 		t_unit = 's' , 
 		coord_unit = 'pxl' , 
 		frames = 0 , 
 		coord = ( 1 , 2 ) , 
 		f = 3 , 
 		protein = 'Sla1-GFP' , 
-		date = '23/01/19' , 
+		date = '23/06/01' , 
 		notes = 'the trajectory of the target protein' , 
 		intensity_normalisation = 'Absolute')
 
@@ -22,16 +22,15 @@ sac6_trajectories = load_directory(
 		path = path_raw_DW_trajectories ,
 		pattern = '.W1data.txt$' ,
 		comment_char = '%' , 
-		dt = 0.2715 , 
+		dt = 0.259 , 
 		t_unit = 's' , 
 		coord_unit = 'pxl' , 
 		frames = 0 , 
 		coord = ( 1 , 2 ) , 
 		f = 3 , 
 		protein = 'Sac6-mCherry' , 
-		date = '23/01/19' , 
+		date = '23/06/01' , 
 		notes = 'the trajectory of the reference protein' ,
 		intensity_normalisation = 'Absolute')
 
 align_raw( path_reference = '../../../Fimbrin/24_degree/Sc/fim1_sc_24deg.txt' , ch1 = sla1_trajectories , ch2 = sac6_trajectories , fimax2 = True )
-#align( path_target = 'sla1_sc_24deg.txt' , path_reference = '../../../Fimbrin/24_degree/Sc/sac6_rotated.txt' , ch1 = sla1_trajectories , ch2 = sac6_trajectories , fimax2 = True )
