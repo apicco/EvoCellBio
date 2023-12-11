8f6283b9be9ad6a2115cc83c581b49f105adc7d0
apicco <andrea.picco@unige.ch>
2023-12-11 16:06:45 +0100

    add log and separate species; grid to be refined

--- Protein_lifetimes/swarm_plots.py
+++ Protein_lifetimes/swarm_plots.py
@@ -9,7 +9,7 @@ from funs import *
 is_sc = Is( 'Cerevisiae' , I_sc , dt = 0.7 )
 shift_sc = - is_sc[ 0 ]
 
-# lifetimes
+# add lifetimes to data frame df
 df = slt( '$S. cerevisiae$' , 'Eps15 (Ede1)' , Ede1_sc , dt = 1.2 , shift = shift_sc )
 df = slt( '$S. cerevisiae$' , 'Pan1' , Pan1_sc , dt = 1.2 , df = df ,  shift = shift_sc )
 df = slt( '$S. cerevisiae$' , 'Sla1' , Sla1_sc , dt = 1.2 , df = df , shift = shift_sc )
@@ -20,6 +20,7 @@ df = slt( '$S. cerevisiae$' , 'Rvs167' , Rvs_sc , dt = 1.19 , df = df , shift = 
 df = slt( '$S. cerevisiae$' , 'Fim1' , Fim1_GFP_sc , dt = 1.2 , df = df , shift = shift_sc , is_t0 = False )
 
 print( df )
+
 # invagination start
 is_sp = Is( 'Pombe' , I_sp , dt = 0.71 )
 shift_sp = - is_sp[ 0 ]
@@ -45,10 +46,13 @@ df = slt( '$U. maydis$' , 'Myo I (Myo5)' , Myo1_um , dt = 1.2 , df = df , shift 
 df = slt( '$U. maydis$' , 'Rvs167' , Rvs_um , dt = 1.19 , df = df , shift = shift_um )
 df = slt( '$U. maydis$' , 'Fim1' , Fim1_GFP_um , dt = 1.2 , df = df , shift = shift_um , is_t0 = False )
 
+print( df[ 'Species'  ] )
 
 order = [ 'Eps15 (Ede1)' , 'Eps15 (Ucp8)' , 'Pan1' , 'Sla1' , 'Wasp' , 'Myo I (Myo5)' , 'Myo I (Myo3)' , 'Fim1'  , 'Rvs167']
 
 fig = plt.figure( figsize = ( 6.5 , 4 ) , tight_layout = True )
-sns.stripplot( data=df , x = "Lifetime (s)" , y = "Protein" , hue = "Species" , dodge = False , order = order , size = 7 , alpha = .3 , jitter=0.38 , marker = 'X' )#, palette = [ '#9DD9D2' , '#CA2E55' , '#F9A03F' ] )
-plt.grid( axis = 'x' )
+sns.stripplot( data=df , x = "Lifetime (s)" , y = "Protein" , hue = "Species" , dodge = True , order = order , size = 7 , alpha = .3 , jitter=0.38 , marker = 'o' )#, palette = [ '#9DD9D2' , '#CA2E55' , '#F9A03F' ] )
+#plt.grid( ydata = [ 2:5 ] )# axis = 'x' )
+plt.xlim( [ 1 , 500 ] )
+plt.xscale( 'log' )
 plt.savefig( 'swarmplot.pdf' )
