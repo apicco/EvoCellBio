1a80ff73e89590e2ddc2e3d09162f5f8d623b629
Andrea Picco <apicco@gmail.com>
2024-08-25 18:59:22 +0200

    reorganise plot.py to plot other species

--- Turgor_pressure/plot.py
+++ Turgor_pressure/plot.py
@@ -2,53 +2,15 @@
 import sys
 sys.path.append( '../' )
 
-import numpy as np
-import pandas as pd
-import matplotlib as mpl
-import statsmodels.api as sm
+from funs import *
 
-mpl.use('Agg')
-from matplotlib import pyplot as plt
-from sklearn.linear_model import LinearRegression
-
+# UM
 um_0p25M = pd.read_csv( 'Data/UM/0p25M.csv' )
 um_0p50M = pd.read_csv( 'Data/UM/0p50M.csv' )
 um_0p75M = pd.read_csv( 'Data/UM/0p75M.csv' )
 um_1M = pd.read_csv( 'Data/UM/1M.csv' )
 um_vol = pd.read_csv( 'Data/UM/um_volumes.csv' )
 
-def xtract( c , df ) :
-
-    x = df.mean( numeric_only=True )[ 'Real Volume' ]
-    e = 1.96 * df.sem( numeric_only=True )[ 'Real Volume' ]
-
-    d = { 'Volume' : x , 'Sem' : e }
-    return pd.DataFrame( data = d , index = [c] )
-
-def plot( d , label  , color ) :
-    c = d.index
-    x = d[ 'Volume' ]
-    e = d[ 'Sem' ]
-
-    plt.errorbar( c , x , e , marker = 'o' , capsize = 4 , linestyle = '' , alpha = 0.5 , color = color )
-    plt.scatter( c , x , label = label , color = color )
-
-def pressure( x , x_se , coef , coef_se , T ) :
-
-    # concentration
-    print( coef )
-    c = coef[1] / ( x - coef[0] )
-    # error
-    e = np.sqrt( 
-        ( coef_se[1] / ( x - coef[0] ) ) ** 2 +
-        ( coef[1] * x_se / ( x - coef[0] ) ** 2 ) ** 2 +
-        ( coef[1] * coef_se[0] / ( x - coef[0] ) ** 2 ) ** 2 
-    )
-    
-    RT = 8.314 * T 
-
-    return c , e , c * RT / 1000 , e * RT / 1000
- 
 # volumes in sorbitol
 um_d = xtract( 1/0.25 , um_0p25M )
 um_d = pd.concat( [ um_d , xtract( 1/0.5 , um_0p50M ) ] )
@@ -58,32 +20,15 @@ um_d = pd.concat( [ um_d , xtract( 1/1 , um_1M ) ] )
 # volume of WT cells at 24C
 um_v = xtract( 0 , um_vol )
 
-# Linear regressions
+um_model , um_result = fit( um_d )
 
-X = um_d.index.values[1:4].reshape(-1,1)
-Y = um_d.values[1:4,0].reshape(-1,1)
-
-model = LinearRegression()
-model.fit( X , Y )
-
-# Compute concentration with statsmodel
-X = sm.add_constant( X )
-result = sm.OLS( Y , X ).fit() 
-
-print( result.summary() )
-coef = result.params
-print( coef )
-coef_se = result.bse
-print( np.array( um_v[ 'Volume' ] ).reshape( 1,-1 ) )
-print( np.array( um_v[ 'Volume' ] ) )
-c = 1/result.predict( np.append( 1 ,  um_v[ 'Volume' ] ) )
-
+T = 297.15 #Kelvin
 P = pressure( 
     np.array( um_v[ 'Volume' ] ) , 
     np.array( um_v[ 'Sem' ] ) , 
-    coef , 
-    coef_se ,
-    T = 297.15 )
+    um_result.params , 
+    um_result.bse ,
+    T = T )
 
 f = plt.figure()
 fs = 13
@@ -97,7 +42,7 @@ plt.plot( xlim , [ um_v[ 'Volume' ] ] * 2 , '-' , color = 'pink' )
 plt.plot( xlim , [ um_v[ 'Volume' ] - um_v[ 'Sem' ] ] * 2 , '--' , color = 'pink' )
 plt.plot( xlim , [ um_v[ 'Volume' ] + um_v[ 'Sem' ] ] * 2 , '--' , color = 'pink' )
 
-Y_pred = model.predict( np.array( xlim ).reshape(-1,1) )
+Y_pred = um_model.predict( np.array( xlim ).reshape(-1,1) )
 plt.plot( xlim , Y_pred , '-' , color = 'red' )
 plt.xlabel( '(Sorbitol concentration)$^{-1}$ / $M^{-1}$' , fontsize = fs , style = sty )
 plt.ylabel( "Volume / $\mu m^3$" , fontsize = fs , style = sty )
