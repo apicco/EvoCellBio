4648c2c51f2c55e52897008e4dc88ce22a209a44
Andrea Picco <apicco@gmail.com>
2024-09-04 00:15:02 +0200

    add SP data

--- Turgor_pressure/plot.py
+++ Turgor_pressure/plot.py
@@ -26,6 +26,17 @@ sc_d = pd.concat( [ sc_d , xtract( 1/0.5 , sc_0p50M ) ] )
 sc_d = pd.concat( [ sc_d , xtract( 1/0.75 , sc_0p75M ) ] )
 sc_d = pd.concat( [ sc_d , xtract( 1/1 , sc_1M ) ] )
 
+# sp
+sp_0p25M = pd.read_csv( 'Data/SP/0p25M.csv' )
+sp_0p50M = pd.read_csv( 'Data/SP/0p50M.csv' )
+sp_0p75M = pd.read_csv( 'Data/SP/0p75M.csv' )
+sp_1M = pd.read_csv( 'Data/SP/1M.csv' )
+sp_d = xtract( 1/0.25 , sp_0p25M )
+sp_d = pd.concat( [ sp_d , xtract( 1/0.5 , sp_0p50M ) ] )
+sp_d = pd.concat( [ sp_d , xtract( 1/0.75 , sp_0p75M ) ] )
+sp_d = pd.concat( [ sp_d , xtract( 1/1 , sp_1M ) ] )
+
+
 # volume of WT cells at 24C
 # um
 um_vol = pd.read_csv( 'Data/UM/um_volumes.csv' )
@@ -33,12 +44,17 @@ um_v = xtract( 0 , um_vol )
 # sc
 sc_vol = pd.read_csv( 'Data/SC/sc_volumes.csv' )
 sc_v = xtract( 0 , sc_vol )
+# sp
+sp_vol = pd.read_csv( 'Data/SP/sp_volumes.csv' )
+sp_v = xtract( 0 , sp_vol )
 
 # fits
 print( 'um' )
 um_model , um_result = fit( um_d )
 print( 'sc' )
 sc_model , sc_result = fit( sc_d )
+print( 'sp' )
+sp_model , sp_result = fit( sp_d )
 
 # pressure
 T = 297.15 #Kelvin
@@ -56,6 +72,13 @@ sc_P = pressure(
     sc_result.params , 
     sc_result.bse ,
     T = T )
+# sp
+sp_P = pressure( 
+    np.array( sp_v[ 'Volume' ] ) , 
+    np.array( sp_v[ 'Sem' ] ) , 
+    sp_result.params , 
+    sp_result.bse ,
+    T = T )
 
 # plot to pdf
 f = plt.figure()
@@ -68,6 +91,8 @@ um_color = 'red'
 um_label = 'U. maydis\n'
 sc_color = 'blue'
 sc_label = 'S. cerevisiae\n'
+sp_color = 'green'
+sp_label = 'S. pombe\n'
 
 # um
 um_stri =  "%.2f MPa $\pm$ %.2f MPa" %  ( um_P[2] , um_P[3] )
@@ -90,6 +115,18 @@ plt.plot( xlim , [ sc_v[ 'Volume' ] + sc_v[ 'Sem' ] ] * 2 , '--' , color = sc_co
 
 sc_Y_pred = sc_model.predict( np.array( xlim ).reshape(-1,1) )
 plt.plot( xlim , sc_Y_pred , '-' , color = sc_color )
+
+# sp
+sp_stri =  "%.2f MPa $\pm$ %.2f MPa" %  ( sp_P[2] , sp_P[3] )
+plot( sp_d  , sp_label + sp_stri , color = sp_color )
+
+plt.plot( xlim , [ sp_v[ 'Volume' ] ] * 2 , '-' , color = sp_color , alpha = 0.4 )
+plt.plot( xlim , [ sp_v[ 'Volume' ] - sp_v[ 'Sem' ] ] * 2 , '--' , color = sp_color , alpha = 0.4 )
+plt.plot( xlim , [ sp_v[ 'Volume' ] + sp_v[ 'Sem' ] ] * 2 , '--' , color = sp_color , alpha = 0.4 )
+
+sp_Y_pred = sp_model.predict( np.array( xlim ).reshape(-1,1) )
+plt.plot( xlim , sp_Y_pred , '-' , color = sp_color )
+
 
 
 plt.xlabel( '(Sorbitol concentration)$^{-1}$ / $M^{-1}$' , fontsize = fs , style = sty )
