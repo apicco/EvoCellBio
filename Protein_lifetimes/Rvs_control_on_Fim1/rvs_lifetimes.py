63f77d1832eeb122e6a936e850b4c3e8922777fb
Andrea Picco <apicco@gmail.com>
2025-01-27 23:22:04 +0100

    rvs in sc

--- Protein_lifetimes/Rvs_control_on_Fim1/rvs_lifetimes.py
+++ Protein_lifetimes/Rvs_control_on_Fim1/rvs_lifetimes.py
@@ -15,11 +15,13 @@ Rvs_no_Fim1_sp = pd.read_csv( 'rvs_only_in_Sp.csv' )
 #Rvs167
 
 Rvs_um = pd.read_csv( "../Lifetime_measurements/Rvs167_lifetimes_Um.csv" )
-Rvs_sc = pd.read_csv( "../Lifetime_measurements/Rvs167_lifetimes_Sc.csv" )
+#Rvs_sc = pd.read_csv( "../Lifetime_measurements/Rvs167_lifetimes_Sc.csv" )
+Rvs_sc = pd.read_csv( "rvs_with_Fim1_in_Sc.csv" )
 Rvs_sp = pd.read_csv( "../Lifetime_measurements/Rvs167_lifetimes_Sp.csv" )
 
+print(Rvs_sc)
 species = 'S. cerevisiae'
-ref ,  data = rlt( species , 'Rvs167' , Rvs_no_Fim1_sc , dt = 1.2577 )
+ref ,  data = rlt( species , 'Rvs167' , Rvs_no_Fim1_sc , dt = 1.257 )
 _ ,  tmp = rlt( species , 'Rvs167 with Fim1-mCherry' , Rvs_sc , dt = 1.19 , ref = ref )
 data = pd.concat( [ data , tmp ] )
 species = 'S. pombe'
