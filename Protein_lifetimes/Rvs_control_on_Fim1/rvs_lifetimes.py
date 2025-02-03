2551576b49dd5db56ee109c3e054e19047af2040
Andrea Picco <apicco@gmail.com>
2025-02-03 22:52:28 +0100

    add rvs lifetimes in um without fim1-mcherry to complete the control

--- Protein_lifetimes/Rvs_control_on_Fim1/rvs_lifetimes.py
+++ Protein_lifetimes/Rvs_control_on_Fim1/rvs_lifetimes.py
@@ -11,7 +11,7 @@ from matplotlib import pyplot as plt
 
 Rvs_no_Fim1_sc = pd.read_csv( 'rvs_only_in_Sc.csv' )
 Rvs_no_Fim1_sp = pd.read_csv( 'rvs_only_in_Sp.csv' )
-
+Rvs_no_Fim1_um = pd.read_csv( 'rvs_only_in_Um.csv' )
 #Rvs167
 
 Rvs_um = pd.read_csv( "../Lifetime_measurements/Rvs167_lifetimes_Um.csv" )
@@ -19,7 +19,6 @@ Rvs_um = pd.read_csv( "../Lifetime_measurements/Rvs167_lifetimes_Um.csv" )
 Rvs_sc = pd.read_csv( "rvs_with_Fim1_in_Sc.csv" )
 Rvs_sp = pd.read_csv( "../Lifetime_measurements/Rvs167_lifetimes_Sp.csv" )
 
-print(Rvs_sc)
 species = 'S. cerevisiae'
 ref ,  data = rlt( species , 'Rvs167' , Rvs_no_Fim1_sc , dt = 1.257 )
 _ ,  tmp = rlt( species , 'Rvs167 with Fim1-mCherry' , Rvs_sc , dt = 1.19 , ref = ref )
@@ -28,5 +27,10 @@ species = 'S. pombe'
 ref ,  tmp = rlt( species , 'Rvs167' , Rvs_no_Fim1_sp , dt = 1.2577 )
 data = pd.concat( [ data , tmp ] )
 _ ,  tmp = rlt( species , 'Rvs167 with Fim1-mCherry' , Rvs_sp , dt = 1.19 , ref = ref )
+data = pd.concat( [ data , tmp ] )
+species = 'U. maydis'
+ref ,  tmp = rlt( species , 'Rvs167' , Rvs_no_Fim1_um , dt = 1.255 )
+data = pd.concat( [ data , tmp ] )
+_ ,  tmp = rlt( species , 'Rvs167 with Fim1-mCherry' , Rvs_um , dt = 1.19 , ref = ref )
 data = pd.concat( [ data , tmp ] )
 print( data )
