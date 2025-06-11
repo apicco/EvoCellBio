bcb0b29b25ad7536a09dd33983705449e48596f7
Andrea Picco <apicco@gmail.com>
2025-06-11 22:25:05 +0200

    correct t0 that was slightly off in plot_aligned.pdf

--- Protein_tracking/plot_aligned.py
+++ Protein_tracking/plot_aligned.py
@@ -26,41 +26,41 @@ axes = fig.subplot_mosaic(
 # 24 degree dynamics
 # S. cerevisiae
 
-myplot( axes["A"] , fim1_sc_24_dw_average , what = 'coord' , label = 'fimbrin' , col = color_Fim1 , x_scale = 100 , add_CI = False )
-myplot( axes["A"] , sla1_sc_24_dw_average , what = 'coord' , label = 'Sla1' , col = color_Sla1 , x_scale = 100 , add_CI = False )
-myplot( axes["A"] , rvs167_sc_24_dw_average , what = 'coord' , label = 'Rvs167' , col = color_Rvs , x_scale = 100 , add_CI = False )
+myplot( axes["A"] , fim1_sc_24_dw_average , what = 'coord' , label = 'fimbrin' , col = color_Fim1 , x_scale = 100 , add_CI = False , t0 = fim1_sc_24_dw_average.t(0) )
+myplot( axes["A"] , sla1_sc_24_dw_average , what = 'coord' , label = 'Sla1' , col = color_Sla1 , x_scale = 100 , add_CI = False , t0 = fim1_sc_24_dw_average.t(0) )
+myplot( axes["A"] , rvs167_sc_24_dw_average , what = 'coord' , label = 'Rvs167' , col = color_Rvs , x_scale = 100 , add_CI = False , t0 = fim1_sc_24_dw_average.t(0) )
 layout( axes[ "A"] , tlim_alignment , movlim_dw["A"] , '$S. cerevisiae$' , legend = True , legend_title = "Protein:" , xaxis_label = False )
 
 # S. pombe
-myplot( axes["B"] , fim1_sp_24_dw_average , what = 'coord' , label = 'fimbrin' , col = color_Fim1 , x_scale = 100 , add_CI = False )
-myplot( axes["B"] , sla1_sp_24_dw_average , what = 'coord' , label = 'Sla1' , col = color_Sla1 , x_scale = 100 , add_CI = False )
-myplot( axes["B"] , rvs167_sp_24_dw_average , what = 'coord' , label = 'Rvs167' , col = color_Rvs , x_scale = 100 , add_CI = False )
+myplot( axes["B"] , fim1_sp_24_dw_average , what = 'coord' , label = 'fimbrin' , col = color_Fim1 , x_scale = 100 , add_CI = False , t0 = fim1_sp_24_dw_average.t(0) )
+myplot( axes["B"] , sla1_sp_24_dw_average , what = 'coord' , label = 'Sla1' , col = color_Sla1 , x_scale = 100 , add_CI = False , t0 = fim1_sp_24_dw_average.t(0) )
+myplot( axes["B"] , rvs167_sp_24_dw_average , what = 'coord' , label = 'Rvs167' , col = color_Rvs , x_scale = 100 , add_CI = False , t0 = fim1_sp_24_dw_average.t(0) )
 layout( axes[ "B"] , tlim_alignment , movlim_dw["A"] , '$S. pombe$' , legend = False , xaxis_label = False , yaxis_label = False )
 
 # U. maydis
-myplot( axes["C"] , fim1_um_24_dw_average , what = 'coord' , label = 'fimbrin' , col = color_Fim1 , x_scale = 100 , add_CI = False )
-myplot( axes["C"] , sla1_um_24_dw_average , what = 'coord' , label = 'Sla1' , col = color_Sla1 , x_scale = 100 , add_CI = False )
-myplot( axes["C"] , rvs167_um_24_dw_average , what = 'coord' , label = 'Rvs167' , col = color_Rvs , x_scale = 100 , add_CI = False )
+myplot( axes["C"] , fim1_um_24_dw_average , what = 'coord' , label = 'fimbrin' , col = color_Fim1 , x_scale = 100 , add_CI = False , t0 = fim1_um_24_dw_average.t(0) )
+myplot( axes["C"] , sla1_um_24_dw_average , what = 'coord' , label = 'Sla1' , col = color_Sla1 , x_scale = 100 , add_CI = False , t0 = fim1_um_24_dw_average.t(0) )
+myplot( axes["C"] , rvs167_um_24_dw_average , what = 'coord' , label = 'Rvs167' , col = color_Rvs , x_scale = 100 , add_CI = False , t0 = fim1_um_24_dw_average.t(0) )
 layout( axes[ "C"] , tlim_alignment , movlim_dw["A"] , '$U. maydis$' , legend = False , xaxis_label = False , yaxis_label = False )
 
 # 24 degree FI
 # S. cerevisiae
-myplot( axes["D"] , fim1_sc_24_dw_average , what = 'f' , label = 'fimbrin' , col = color_Fim1 , add_CI = False )
-myplot( axes["D"] , sla1_sc_24_dw_average , what = 'f' , label = 'Sla1' , col = color_Sla1 , add_CI = False )
-myplot( axes["D"] , rvs167_sc_24_dw_average , what = 'f' , label = 'Rvs167' , col = color_Rvs , add_CI = False )
+myplot( axes["D"] , fim1_sc_24_dw_average , what = 'f' , label = 'fimbrin' , col = color_Fim1 , add_CI = False , t0 = fim1_sc_24_dw_average.t(0) )
+myplot( axes["D"] , sla1_sc_24_dw_average , what = 'f' , label = 'Sla1' , col = color_Sla1 , add_CI = False , t0 = fim1_sc_24_dw_average.t(0) )
+myplot( axes["D"] , rvs167_sc_24_dw_average , what = 'f' , label = 'Rvs167' , col = color_Rvs , add_CI = False , t0 = fim1_sc_24_dw_average.t(0) )
 layout( axes[ "D"] , tlim_alignment , flim , ylabel = 'Fluor. int. / a.u.' , title = None )
 
 # S. pombe
-myplot( axes["E"] , fim1_sp_24_dw_average , what = 'f' , label = 'fimbrin' , col = color_Fim1 , add_CI = False )
-myplot( axes["E"] , sla1_sp_24_dw_average , what = 'f' , label = 'Sla1' , col = color_Sla1 , add_CI = False )
-myplot( axes["E"] , rvs167_sp_24_dw_average , what = 'f' , label = 'Rvs167' , col = color_Rvs , add_CI = False )
+myplot( axes["E"] , fim1_sp_24_dw_average , what = 'f' , label = 'fimbrin' , col = color_Fim1 , add_CI = False , t0 = fim1_sp_24_dw_average.t(0) )
+myplot( axes["E"] , sla1_sp_24_dw_average , what = 'f' , label = 'Sla1' , col = color_Sla1 , add_CI = False , t0 = fim1_sp_24_dw_average.t(0) )
+myplot( axes["E"] , rvs167_sp_24_dw_average , what = 'f' , label = 'Rvs167' , col = color_Rvs , add_CI = False , t0 = fim1_sp_24_dw_average.t(0) )
 layout( axes[ "E"] , tlim_alignment , flim , yaxis_label = False , title = None )
 
 # U. maydis
-myplot( axes["F"] , fim1_um_24_dw_average , what = 'f' , label = 'fimbrin' , col = color_Fim1 , add_CI = False )
-myplot( axes["F"] , sla1_um_24_dw_average , what = 'f' , label = 'Sla1' , col = color_Sla1 , add_CI = False )
-myplot( axes["F"] , rvs167_um_24_dw_average , what = 'f' , label = 'Rvs167' , col = color_Rvs , add_CI = False )
-#myplot( axes["F"] , fim1_um_24_aligned_average , what = 'f' , label = 'Rvs167' , col = color_Fim1 , add_CI = False )
+myplot( axes["F"] , fim1_um_24_dw_average , what = 'f' , label = 'fimbrin' , col = color_Fim1 , add_CI = False , t0 = fim1_um_24_dw_average.t(0) )
+myplot( axes["F"] , sla1_um_24_dw_average , what = 'f' , label = 'Sla1' , col = color_Sla1 , add_CI = False , t0 = fim1_um_24_dw_average.t(0) )
+myplot( axes["F"] , rvs167_um_24_dw_average , what = 'f' , label = 'Rvs167' , col = color_Rvs , add_CI = False , t0 = fim1_um_24_dw_average.t(0) )
+#myplot( axes["F"] , fim1_um_24_aligned_average , what = 'f' , label = 'Rvs167' , col = color_Fim1 , add_CI = False , t0 = fim1_um_24_dw_average.t(0))
 layout( axes[ "F"] , tlim_alignment , flim , yaxis_label = False , title = None )
 
 fig.savefig( 'plot_aligned.pdf')
